33c266bcd9b54a1ea676cf0c7dea12f8caf0ee08
rechrtb <rechrtb@gmail.com>
2024-01-26 17:43:16 +0800

    Move hw_ funcs before all hcd_ funcs

--- src/portable/microchip/samx7x/hcd_samx7x.c
+++ src/portable/microchip/samx7x/hcd_samx7x.c
@@ -153,7 +153,6 @@ static uint8_t hw_pipe_find(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr)
   return EP_MAX;
 }
 
-
 static uint8_t hw_pipe_find_free(uint8_t rhport)
 {
   (void)rhport;
@@ -448,120 +447,6 @@ static void hw_handle_dma_int(uint8_t rhport, uint32_t isr)
   return;
 }
 
-bool hcd_init(uint8_t rhport)
-{
-  add_evt(12);
-  (void)rhport;
-  hcd_int_disable(rhport);
-
-  //// system_init -> USB_HOST_INSTANCE_init -> USB_HOST_INSTANCE_CLOCK_init
-  // Note: already done in board_init()
-
-  //// system_init -> USB_HOST_INSTANCE_init -> usb_h_init ->  _usb_h_init
-  // 	struct _usb_h_prvt *pd = (struct _usb_h_prvt *)prvt;
-
-  // 	ASSERT(drv && hw && prvt && pd->pipe_pool && pd->pipe_pool_size);
-  // 	if (hri_usbhs_get_CTRL_reg(hw, USBHS_CTRL_USBE)) {
-  // 		return USB_H_DENIED;
-  // 	}
-
-  // #if CONF_USB_H_VBUS_CTRL
-  // 	CONF_USB_H_VBUS_CTRL_FUNC(drv, 1, false);
-  // #endif
-
-  // 	drv->sof_cb = (usb_h_cb_sof_t)_dummy_func_no_return;
-  // 	drv->rh_cb  = (usb_h_cb_roothub_t)_dummy_func_no_return;
-
-  // 	drv->prvt = prvt;
-  // 	drv->hw   = hw;
-
-  // 	_usb_h_reset_pipe_xfers(drv, false);
-  hw_pipes_reset(rhport);
-
-  // 	pd->suspend_start   = 0;
-  // 	pd->resume_start    = 0;
-  // 	pd->pipe_xfers_unfreeze  = 0;
-  // 	pd->n_ctrl_req_user = 0;
-  // 	pd->n_sof_user      = 0;
-  // 	pd->dpram_used      = 0;
-
-  // 	_usb_h_dev = drv;
-
-  // 	NVIC_EnableIRQ(USBHS_IRQn);
-  // Note: done in tuh_init, after this call returns
-
-  // #define USBHS_CTRL_UIDE 0x01000000 /* UOTGID pin enable */
-  // 	hri_usbhs_clear_CTRL_reg(hw, USBHS_CTRL_UIMOD | USBHS_CTRL_UIDE);
-  USB_REG->CTRL &= ~(CTRL_UIMOD | CTRL_UID);
-
-  // 	hri_usbhs_set_CTRL_reg(hw, USBHS_CTRL_FRZCLK);
-  USB_REG->CTRL |= CTRL_FRZCLK;
-
-  // #if CONF_USBHS_SRC == CONF_SRC_USB_48M
-  // 	hri_usbhs_write_HSTCTRL_SPDCONF_bf(hw, 1);
-  // #else
-  // 	hri_usbhs_clear_HSTCTRL_reg(hw, USBHS_HSTCTRL_SPDCONF_Msk);
-  // #endif
-  // TODO: detect if set to use high speed
-  USB_REG->HSTCTRL |= HSTCTRL_SPDCONF;
-  // USB_REG->HSTCTRL &= ~HSTCTRL_SPDCONF;
-
-  // 	/* Force re-connection on initialization */
-  // 	hri_usbhs_write_HSTIFR_reg(drv->hw, USBHS_HSTIMR_DDISCIE | USBHS_HSTIMR_HWUPIE);
-  // TODO: check if this is really correct
-  USB_REG->HSTIFR |= HSTIMR_DDISCIE | HSTIMR_HWUPIE;
-
-  // 	return USB_H_OK;
-
-  //// usbhc_start -> usb_h_enable -> _usb_h_enable
-  // 	ASSERT(drv && drv->hw);
-  // 	hri_usbhs_set_CTRL_reg(drv->hw, USBHS_CTRL_USBE);
-  USB_REG->CTRL = CTRL_USBE;
-  // 	/* Check USB clock */
-  // 	hri_usbhs_clear_CTRL_reg(drv->hw, USBHS_CTRL_FRZCLK);
-  // TODO: Check if this can be unfreezed on interrupt handling like the device implementation.
-  // USB_REG->CTRL &= ~CTRL_FRZCLK;
-  // 	while (!hri_usbhs_get_SR_reg(drv->hw, USBHS_SR_CLKUSABLE));
-  // while (!(USB_REG->SR & SR_CLKUSABLE));
-
-  // 	/* Clear all interrupts that may have been set by a previous host mode */
-  // 	hri_usbhs_write_HSTICR_reg(drv->hw,
-  // 	                           USBHS_HSTICR_DCONNIC | USBHS_HSTICR_HSOFIC | USBHS_HSTICR_RSMEDIC | USBHS_HSTICR_RSTIC
-  // 	                               | USBHS_HSTICR_RXRSMIC);
-
-  USB_REG->HSTICR |= (HSTICR_DCONNIC | HSTICR_HSOFIC | HSTICR_RSMEDIC | HSTICR_RSTIC | HSTICR_RXRSMIC);
-
-  // 	/* VBus Hardware Control! */
-  // 	hri_usbhs_set_CTRL_reg(drv->hw, 1u << 8);
-  USB_REG->CTRL |= CTRL_VBUSHWC; // datasheet indicates must be set to 1
-                                 // #if CONF_USB_H_VBUS_CTRL
-                                 // 	CONF_USB_H_VBUS_CTRL_FUNC(drv, 1, true);
-                                 // #endif
-
-  // 	/* Enable interrupts to detect connection */
-  // 	hri_usbhs_set_HSTIMR_reg(drv->hw,
-  // 	                         USBHS_HSTIMR_DCONNIE | USBHS_HSTIMR_RSTIE | USBHS_HSTIMR_HSOFIE | USBHS_HSTIMR_HWUPIE);
-  USB_REG->HSTIER |= HSTIER_HWUPIES;
-
-  ready = false;
-
-  return true;
-}
-
-void hcd_device_close(uint8_t rhport, uint8_t dev_addr)
-{
-  add_evt(11);
-  // Reset every pipe associated with the device
-  for (uint8_t i = 0; i < EP_MAX; i++)
-  {
-    if (hw_pipe_enabled(rhport, i) && hw_pipe_get_dev_addr(rhport, i) == dev_addr)
-    {
-      hw_pipe_reset(rhport, i);
-      USB_REG->HSTPIP &= ~(((1 << i) << HSTPIP_PEN_Pos) & HSTPIP_PEN); // disable pipe
-    }
-  }
-}
-
 static bool hw_pipe_setup_dma(uint8_t rhport, uint8_t pipe, bool end)
 {
   uint32_t flags = 0;
@@ -734,6 +619,169 @@ static bool hw_pipe_setup_dma(uint8_t rhport, uint8_t pipe, bool end)
   add_evt(10010);
 
   return false;
+}
+
+static void hw_handle_rh_int(uint8_t rhport, uint32_t isr)
+{
+  // Device reset
+  if (isr & HSTISR_RSTI)
+  {
+    USB_REG->HSTICR |= HSTICR_RSTIC;
+    USB_REG->HSTIDR |= HSTIDR_RSTIEC;
+    ready = true;
+    return;
+  }
+
+  // Device disconnection
+  if (isr & HSTISR_DDISCI)
+  {
+    USB_REG->HSTICR = HSTICR_DDISCIC | HSTICR_DCONNIC;
+    USB_REG->HSTIDR = HSTIDR_DDISCIEC | HSTIDR_HWUPIEC | HSTIDR_RSMEDIEC | HSTIDR_RXRSMIEC;
+    USB_REG->HSTCTRL &= ~HSTCTRL_RESET;
+    USB_REG->HSTICR = HSTICR_DCONNIC | HSTICR_HWUPIC | HSTICR_RSMEDIC | HSTICR_RXRSMIC;
+    USB_REG->HSTIER = HSTIER_DCONNIES | HSTIER_HWUPIES | HSTIER_RSMEDIES | HSTIER_RXRSMIES;
+    hcd_event_device_remove(rhport, true);
+    return;
+  }
+
+  // Device connection
+  if (isr & HSTISR_DCONNI)
+  {
+    USB_REG->HSTICR |= HSTICR_DCONNIC;
+    USB_REG->HSTIDR |= HSTISR_DCONNI;
+    USB_REG->HSTIER |= HSTIER_DDISCIES;
+    USB_REG->HSTCTRL |= HSTCTRL_SOFE;
+    hcd_event_device_attach(rhport, true);
+    return;
+  }
+
+  // Host wakeup
+  if (isr & HSTISR_HWUPI)
+  {
+    USB_REG->CTRL &= ~CTRL_FRZCLK;
+    while (!(USB_REG->SR & SR_CLKUSABLE));
+
+    USB_REG->HSTIDR |= HSTIDR_HWUPIEC;
+    USB_REG->SFR |= SFR_VBUSRQS;
+    USB_REG->HSTICR |= HSTICR_HWUPIC;
+    USB_REG->HSTIDR |= HSTIDR_HWUPIEC;
+    USB_REG->HSTIER |= HSTIER_DCONNIES;
+    return;
+  }
+}
+
+bool hcd_init(uint8_t rhport)
+{
+  add_evt(12);
+  (void)rhport;
+  hcd_int_disable(rhport);
+
+  //// system_init -> USB_HOST_INSTANCE_init -> USB_HOST_INSTANCE_CLOCK_init
+  // Note: already done in board_init()
+
+  //// system_init -> USB_HOST_INSTANCE_init -> usb_h_init ->  _usb_h_init
+  // 	struct _usb_h_prvt *pd = (struct _usb_h_prvt *)prvt;
+
+  // 	ASSERT(drv && hw && prvt && pd->pipe_pool && pd->pipe_pool_size);
+  // 	if (hri_usbhs_get_CTRL_reg(hw, USBHS_CTRL_USBE)) {
+  // 		return USB_H_DENIED;
+  // 	}
+
+  // #if CONF_USB_H_VBUS_CTRL
+  // 	CONF_USB_H_VBUS_CTRL_FUNC(drv, 1, false);
+  // #endif
+
+  // 	drv->sof_cb = (usb_h_cb_sof_t)_dummy_func_no_return;
+  // 	drv->rh_cb  = (usb_h_cb_roothub_t)_dummy_func_no_return;
+
+  // 	drv->prvt = prvt;
+  // 	drv->hw   = hw;
+
+  // 	_usb_h_reset_pipe_xfers(drv, false);
+  hw_pipes_reset(rhport);
+
+  // 	pd->suspend_start   = 0;
+  // 	pd->resume_start    = 0;
+  // 	pd->pipe_xfers_unfreeze  = 0;
+  // 	pd->n_ctrl_req_user = 0;
+  // 	pd->n_sof_user      = 0;
+  // 	pd->dpram_used      = 0;
+
+  // 	_usb_h_dev = drv;
+
+  // 	NVIC_EnableIRQ(USBHS_IRQn);
+  // Note: done in tuh_init, after this call returns
+
+  // #define USBHS_CTRL_UIDE 0x01000000 /* UOTGID pin enable */
+  // 	hri_usbhs_clear_CTRL_reg(hw, USBHS_CTRL_UIMOD | USBHS_CTRL_UIDE);
+  USB_REG->CTRL &= ~(CTRL_UIMOD | CTRL_UID);
+
+  // 	hri_usbhs_set_CTRL_reg(hw, USBHS_CTRL_FRZCLK);
+  USB_REG->CTRL |= CTRL_FRZCLK;
+
+  // #if CONF_USBHS_SRC == CONF_SRC_USB_48M
+  // 	hri_usbhs_write_HSTCTRL_SPDCONF_bf(hw, 1);
+  // #else
+  // 	hri_usbhs_clear_HSTCTRL_reg(hw, USBHS_HSTCTRL_SPDCONF_Msk);
+  // #endif
+  // TODO: detect if set to use high speed
+  USB_REG->HSTCTRL |= HSTCTRL_SPDCONF;
+  // USB_REG->HSTCTRL &= ~HSTCTRL_SPDCONF;
+
+  // 	/* Force re-connection on initialization */
+  // 	hri_usbhs_write_HSTIFR_reg(drv->hw, USBHS_HSTIMR_DDISCIE | USBHS_HSTIMR_HWUPIE);
+  // TODO: check if this is really correct
+  USB_REG->HSTIFR |= HSTIMR_DDISCIE | HSTIMR_HWUPIE;
+
+  // 	return USB_H_OK;
+
+  //// usbhc_start -> usb_h_enable -> _usb_h_enable
+  // 	ASSERT(drv && drv->hw);
+  // 	hri_usbhs_set_CTRL_reg(drv->hw, USBHS_CTRL_USBE);
+  USB_REG->CTRL = CTRL_USBE;
+  // 	/* Check USB clock */
+  // 	hri_usbhs_clear_CTRL_reg(drv->hw, USBHS_CTRL_FRZCLK);
+  // TODO: Check if this can be unfreezed on interrupt handling like the device implementation.
+  // USB_REG->CTRL &= ~CTRL_FRZCLK;
+  // 	while (!hri_usbhs_get_SR_reg(drv->hw, USBHS_SR_CLKUSABLE));
+  // while (!(USB_REG->SR & SR_CLKUSABLE));
+
+  // 	/* Clear all interrupts that may have been set by a previous host mode */
+  // 	hri_usbhs_write_HSTICR_reg(drv->hw,
+  // 	                           USBHS_HSTICR_DCONNIC | USBHS_HSTICR_HSOFIC | USBHS_HSTICR_RSMEDIC | USBHS_HSTICR_RSTIC
+  // 	                               | USBHS_HSTICR_RXRSMIC);
+
+  USB_REG->HSTICR |= (HSTICR_DCONNIC | HSTICR_HSOFIC | HSTICR_RSMEDIC | HSTICR_RSTIC | HSTICR_RXRSMIC);
+
+  // 	/* VBus Hardware Control! */
+  // 	hri_usbhs_set_CTRL_reg(drv->hw, 1u << 8);
+  USB_REG->CTRL |= CTRL_VBUSHWC; // datasheet indicates must be set to 1
+                                 // #if CONF_USB_H_VBUS_CTRL
+                                 // 	CONF_USB_H_VBUS_CTRL_FUNC(drv, 1, true);
+                                 // #endif
+
+  // 	/* Enable interrupts to detect connection */
+  // 	hri_usbhs_set_HSTIMR_reg(drv->hw,
+  // 	                         USBHS_HSTIMR_DCONNIE | USBHS_HSTIMR_RSTIE | USBHS_HSTIMR_HSOFIE | USBHS_HSTIMR_HWUPIE);
+  USB_REG->HSTIER |= HSTIER_HWUPIES;
+
+  ready = false;
+
+  return true;
+}
+
+void hcd_device_close(uint8_t rhport, uint8_t dev_addr)
+{
+  add_evt(11);
+  // Reset every pipe associated with the device
+  for (uint8_t i = 0; i < EP_MAX; i++)
+  {
+    if (hw_pipe_enabled(rhport, i) && hw_pipe_get_dev_addr(rhport, i) == dev_addr)
+    {
+      hw_pipe_reset(rhport, i);
+      USB_REG->HSTPIP &= ~(((1 << i) << HSTPIP_PEN_Pos) & HSTPIP_PEN); // disable pipe
+    }
+  }
 }
 
 bool hcd_setup_send(uint8_t rhport, uint8_t dev_addr, uint8_t const setup_packet[8])
@@ -957,54 +1005,7 @@ bool hcd_edpt_xfer(uint8_t rhport, uint8_t dev_addr, uint8_t ep_addr, uint8_t *b
   return true;
 }
 
-static void hw_handle_rh_int(uint8_t rhport, uint32_t isr)
-{
-  // Device reset
-  if (isr & HSTISR_RSTI)
-  {
-    USB_REG->HSTICR |= HSTICR_RSTIC;
-    USB_REG->HSTIDR |= HSTIDR_RSTIEC;
-    ready = true;
-    return;
-  }
 
-  // Device disconnection
-  if (isr & HSTISR_DDISCI)
-  {
-    USB_REG->HSTICR = HSTICR_DDISCIC | HSTICR_DCONNIC;
-    USB_REG->HSTIDR = HSTIDR_DDISCIEC | HSTIDR_HWUPIEC | HSTIDR_RSMEDIEC | HSTIDR_RXRSMIEC;
-    USB_REG->HSTCTRL &= ~HSTCTRL_RESET;
-    USB_REG->HSTICR = HSTICR_DCONNIC | HSTICR_HWUPIC | HSTICR_RSMEDIC | HSTICR_RXRSMIC;
-    USB_REG->HSTIER = HSTIER_DCONNIES | HSTIER_HWUPIES | HSTIER_RSMEDIES | HSTIER_RXRSMIES;
-    hcd_event_device_remove(rhport, true);
-    return;
-  }
-
-  // Device connection
-  if (isr & HSTISR_DCONNI)
-  {
-    USB_REG->HSTICR |= HSTICR_DCONNIC;
-    USB_REG->HSTIDR |= HSTISR_DCONNI;
-    USB_REG->HSTIER |= HSTIER_DDISCIES;
-    USB_REG->HSTCTRL |= HSTCTRL_SOFE;
-    hcd_event_device_attach(rhport, true);
-    return;
-  }
-
-  // Host wakeup
-  if (isr & HSTISR_HWUPI)
-  {
-    USB_REG->CTRL &= ~CTRL_FRZCLK;
-    while (!(USB_REG->SR & SR_CLKUSABLE));
-
-    USB_REG->HSTIDR |= HSTIDR_HWUPIEC;
-    USB_REG->SFR |= SFR_VBUSRQS;
-    USB_REG->HSTICR |= HSTICR_HWUPIC;
-    USB_REG->HSTIDR |= HSTIDR_HWUPIEC;
-    USB_REG->HSTIER |= HSTIER_DCONNIES;
-    return;
-  }
-}
 
 void hcd_int_handler(uint8_t rhport)
 {
