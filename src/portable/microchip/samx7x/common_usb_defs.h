5c542e16c5bd26587ff109f1d3adbd978c4b1f75
rechrtb <rechrtb@gmail.com>
2024-07-10 19:19:27 +0900

    Update implementation
    - Add dma support
    - Update with asf implementation

--- src/portable/microchip/samx7x/common_usb_defs.h
+++ src/portable/microchip/samx7x/common_usb_defs.h
@@ -35,7 +35,6 @@
 #include "common_usb_regs.h"
 
 #define PEP_GET_FIFO_PTR(ep, scale) (((TU_XSTRCAT(TU_STRCAT(uint, scale),_t) (*)[0x8000 / ((scale) / 8)])FIFO_RAM_ADDR)[(ep)])
-#define DMA_TRANS_MAX   0x10000
 #define PIPE_MAX_PACKET_SIZE 1024
 
 
@@ -52,11 +51,86 @@ TU_ATTR_ALWAYS_INLINE static inline void hw_exit_critical(volatile uint32_t *ato
 	__set_PRIMASK(*atomic);
 }
 
-TU_ATTR_ALWAYS_INLINE static inline void hw_cache_invalidate(uint32_t *addr, int32_t size)
+
+TU_ATTR_ALWAYS_INLINE static inline void hw_dcache_invalidate_prepare(uint8_t *addr, int32_t size)
 {
   if (SCB->CCR & SCB_CCR_DC_Msk)
   {
-    SCB_CleanInvalidateDCache_by_Addr(addr, size);
+    int32_t op_size = size;
+    uint32_t op_addr = (uint32_t) addr;
+    uint32_t addr1 = op_addr;
+    uint32_t addr2 = op_addr + op_size;
+    if (addr1 & 0x1F || addr2 & 0x1F) {
+      addr1 &= ~0x1F;
+      addr2 &= ~0x1F;
+      __ISB();
+      __DSB();
+      SCB->DCCIMVAC = addr1;
+      SCB->DCCIMVAC = addr2;
+      __DSB();
+      __ISB();
+    }
+  }
+  else
+  {
+    __DSB();
+    __ISB();
+  }
+}
+
+TU_ATTR_ALWAYS_INLINE static inline void hw_dcache_invalidate(uint8_t *addr, int32_t size)
+{
+  if (SCB->CCR & SCB_CCR_DC_Msk)
+  {
+    int32_t op_size = size;
+    uint32_t op_addr = (uint32_t)addr;
+    int32_t linesize = 32U;                // in Cortex-M7 size of cache line is fixed to 8 words (32 bytes)
+    if (op_addr & 0x1F) {
+      op_size += op_addr & 0x1F;
+      op_addr &= ~0x1F;
+    }
+    __ISB();
+    __DSB();
+
+    while (op_size > 0) {
+      /* D-Cache Invalidate by MVA to PoC */
+      (*(volatile uint32_t *)(0xE000EF5C)) = op_addr;
+      op_addr += linesize;
+      op_size -= linesize;
+    }
+
+    __DSB();
+    __ISB();
+  }
+  else
+  {
+    __DSB();
+    __ISB();
+  }
+}
+
+TU_ATTR_ALWAYS_INLINE static inline void hw_dcache_flush(uint8_t *addr, int32_t size)
+{
+  if (SCB->CCR & SCB_CCR_DC_Msk)
+  {
+    int32_t op_size = size;
+    uint32_t op_addr = (uint32_t) addr;
+    int32_t linesize = 32U;                // in Cortex-M7 size of cache line is fixed to 8 words (32 bytes)
+    if (op_addr & 0x1F) {
+      op_size += op_addr & 0x1F;
+      op_addr &= ~0x1F;
+    }
+    __ISB();
+    __DSB();
+
+    while (op_size > 0) {
+      SCB->DCCMVAC = op_addr;
+      op_addr += linesize;
+      op_size -= linesize;
+    }
+
+    __DSB();
+    __ISB();
   }
   else
   {
