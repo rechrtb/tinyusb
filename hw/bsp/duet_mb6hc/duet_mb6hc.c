b48139e755a994332eed5791a899f22f7dcfd7eb
rechrtb <rechrtb@gmail.com>
2024-09-16 20:16:27 +0900

    Remove mb6hc uart implementation

--- hw/bsp/duet_mb6hc/duet_mb6hc.c
+++ hw/bsp/duet_mb6hc/duet_mb6hc.c
@@ -143,11 +143,7 @@ int board_uart_read(uint8_t* buf, int len)
 
 int board_uart_write(void const * buf, int len)
 {
-  // while until previous transfer is complete
-  while(uart_busy) {}
-  uart_busy = true;
-
-  io_write(&edbg_com.io, buf, len);
+  // unimplemented for now
   return len;
 }
 
